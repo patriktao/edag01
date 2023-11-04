6c760953b0de2e96dc5bf79497cc912a36ca160d
Patrik Tao <pa0676ta-s@student.lu.se>
2023-11-04 02:22:34 +0100

    Lab1

--- lab1/intopt.c
+++ lab1/intopt.c
@@ -2,26 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double **make_matrix(int m, int n) {
+double **make_matrix(int m, int n)
+{
      /* (*matrix) points to an array of pointers, and each of the pointers points to an array of integers, forming the rows of the matrix */
      double **matrix;
+
+     /* Allocate memory for the Matrix variable */
      matrix = calloc(m, sizeof(double *));
 
      /* Dynamically allocate memory for each cell */
-     for (int i = 0; i < m; i += 1){
+     for (int i = 0; i < m; i += 1)
+     {
           matrix[i] = calloc(n, sizeof(double));
      }
 
      return matrix;
 }
 
-int main(int argc, char **argv) {
+void printRes(int m, int n, double *coefficients, double *values, double **matrix)
+{
+
+     printf("\nC-coefficients:\n");
+     for (int i = 0; i < m; i++)
+     {
+          printf("%10.3lf", coefficients[i]);
+     }
+
+     printf("\nB-values:\n");
+     for (int i = 0; i < m; i++)
+     {
+          printf("%10.3lf", values[i]);
+     }
+
+     // Print Matrix
+     printf("\nMatrix:\n");
+     for (int i = 0; i < m; i++)
+     {
+          for (int j = 0; j < m; j++)
+          {
+               printf("%10.3lf", matrix[i][j]);
+          }
+          printf("\n");
+     }
+}
+
+int main(int argc, char **argv)
+{
      int m, n;
      // m is number of constraints
      // n is number of decision values
 
      // Read the dimensions of the matrix from the file
-     if (scanf("%d %d", &m, &n) != 2) {
+     if (scanf("%d %d", &m, &n) != 2)
+     {
           printf("Invalid input in the file. Please check the format.\n");
           return 1;
      }
@@ -35,11 +68,14 @@ int main(int argc, char **argv) {
 
      // Create matrix
      double **matrix = make_matrix(m, n);
-
+     
      // Fill matrix
-     for (int i = 0; i < m; i++) {
-          for (int j = 0; j < n; j++) {
-               if (scanf("%lf", &matrix[i][j]) != 1) {
+     for (int i = 0; i < m; i++)
+     {
+          for (int j = 0; j < n; j++)
+          {
+               if (scanf("%lf", &matrix[i][j]) != 1)
+               {
                     printf("Invalid input. Please enter an integer.\n");
                     return 1;
                }
@@ -48,31 +84,16 @@ int main(int argc, char **argv) {
 
      // Read  the c-coefficients
      double b_values[m];
-     for (int i = 0; i < m; i++){
+     for (int i = 0; i < m; i++)
+     {
           scanf("%lf", &b_values[i]);
      }
 
-     printf("\nC-coefficients:\n");
-     for (int i = 0; i < m; i++){
-          printf("%10.3lf", c_coefficients[i]);
-     }
-
-     printf("\nB-values:\n");
-     for (int i = 0; i < m; i++){
-          printf("%10.3lf", b_values[i]);
-     }
-
-     // Print Matrix
-     printf("\nMatrix:\n");
-     for (int i = 0; i < m; i++) {
-          for (int j = 0; j < m; j++){ 
-               printf("%10.3lf", matrix[i][j]);
-          }
-          printf("\n");
-     }
+     printRes(m, n, c_coefficients, b_values, matrix);
 
      // Free the allocated memory from each row and the variable
-     for (int i = 0; i < m; i++){
+     for (int i = 0; i < m; i++)
+     {
           free(matrix[i]);
      }
      free(matrix);
