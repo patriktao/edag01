a3992982b2803525e510ebffbd83cc9d9bf0755d
Patrik Tao <pa0676ta-s@student.lu.se>
2024-11-18 22:50:16 +0100

    udpate intopt

---
+++ rpn/rpn_2.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 10
+#define MAX_LEN 256
+
+void reset_states(int *idx, int *num, bool *building, bool *err)
+{
+     *idx = 0;
+     *num = 0;
+     *building = false;
+     *err = false;
+}
+
+void handle_error(int line, char c)
+{
+     printf("line %d: error at %c\n", line, c);
+}
+
+int main(void)
+{
+     unsigned int stack[N];          // Stack to store numbers and results
+     int idx = 0, num = 0, calc = 0; // Stack index and calculation variable
+     int c;
+     bool building = false;
+     bool err = false;
+     int line = 1;
+
+     while ((c = getchar()) != EOF)
+     {
+          if (err)
+          {                                    // Skip to end of line if error
+               while ((c = getchar()) != '\n') // pass until reaching eof
+               {
+               }
+               line++;
+               reset_states(&idx, &num, &building, &err);
+               continue;
+          }
+
+          if (isdigit(c))
+          {
+               building = true;
+               num = num * 10 + (c - '0');
+               continue;
+          }
+
+          if (building)
+          {
+               if (idx == N)
+               {
+                    printf("line %d: error at %d\n", line, num);
+                    err = true;
+                    continue;
+               }
+               else
+               {
+                    stack[idx++] = num;
+               }
+               num = 0;
+               building = false;
+          }
+
+          if (c == ' ')
+          {
+               continue;
+          }
+          else if (c == '\n')
+          {
+               if (idx != 1)
+               {
+                    printf("line %d: error at \\n\n", line);
+               }
+               else
+               {
+                    printf("line %d: %d\n", line, stack[0]);
+               }
+               line++;
+               reset_states(&idx, &num, &building, &err);
+               continue;
+          }
+          else if (c == '+' || c == '-' || c == '*' || c == '/')
+          {
+               if (idx < 2)
+               {
+                    handle_error(line, c);
+                    err = true;
+                    continue;
+               }
+
+               int op2 = stack[--idx]; // pop 1
+               int op1 = stack[--idx]; // pop 2
+
+               switch (c)
+               {
+               case '+':
+                    calc = op1 + op2;
+                    break;
+               case '-':
+                    calc = op1 - op2;
+                    break;
+               case '*':
+                    calc = op1 * op2;
+                    break;
+               case '/':
+                    if (op2 == 0)
+                    {
+                         printf("line %d: error at /\n", line);
+                         err = true;
+                         continue;
+                    }
+                    calc = op1 / op2;
+                    break;
+               }
+               stack[idx++] = calc; // add res
+          }
+          else
+          {
+               handle_error(line, c);
+               err = true;
+          }
+     }
+     return 0;
+}
